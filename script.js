61e0ba71d9d9746a72182656fd8c220d1991f548
Wish <wishdog2013@hotmail.com>
2023-06-03 18:01:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,120 +1,100 @@
-// Function to generate a random number within a specified range
-function generateRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+// GuessR Number Game
+
+// Global variables
+let secretNumber;
+let attempts;
+let leaderboard = [];
+let loggedInUser = null;
+
+// Generate a random number between 1 and 100
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
 }
 
-// Function to check the user's guess against the random number
-function checkGuess(randomNumber, guess) {
-  if (guess === randomNumber) {
-    return "equal";
-  } else if (guess < randomNumber) {
-    return "lower";
-  } else {
-    return "higher";
-  }
+// Start a new game
+function startNewGame() {
+  secretNumber = generateRandomNumber();
+  attempts = 0;
+  leaderboard = [];
+  loggedInUser = null;
+  clearInput();
+  updateMessage("Guess a number between 1 and 100");
+  updateAttempts();
+  showLeaderboard();
 }
 
-// Example usage
-const minRange = 1;
-const maxRange = 100;
-let randomNumber = generateRandomNumber(minRange, maxRange);
-let attempts = 0;
-let leaderboard = []; // Array to store leaderboard data
-let username = ""; // Variable to store the username
-let password = ""; // Variable to store the password
-
-// Function to handle user input and check the guess
+// Handle guess input
 function handleGuess() {
-  const guess = parseInt(document.getElementById("guessInput").value);
+  const guessInput = document.getElementById("guessInput");
+  const guess = parseInt(guessInput.value);
 
   if (isNaN(guess)) {
-    alert("Invalid input! Please enter a valid number.");
+    updateMessage("Please enter a valid number");
     return;
   }
 
   attempts++;
 
-  const result = checkGuess(randomNumber, guess);
+  if (guess === secretNumber) {
+    updateMessage(`Congratulations! You guessed the number in ${attempts} attempts.`);
+    addLeaderboardEntry();
+    startNewGame();
+  } else if (guess < secretNumber) {
+    updateMessage("Too low! Guess higher.");
+  } else {
+    updateMessage("Too high! Guess lower.");
+  }
 
-  if (result === "equal") {
-    alert(`Congratulations, ${username}! You guessed the number ${randomNumber} in ${attempts} attempts.`);
+  updateAttempts();
+  clearInput();
+}
 
-    // Add the result to the leaderboard
-    leaderboard.push({ username, attempts });
+// Update the message displayed to the player
+function updateMessage(message) {
+  const messageElement = document.getElementById("message");
+  messageElement.textContent = message;
+}
 
-    // Sort the leaderboard based on the number of attempts (ascending order)
+// Update the number of attempts displayed to the player
+function updateAttempts() {
+  const attemptsElement = document.getElementById("attempts");
+  attemptsElement.textContent = `Attempts: ${attempts}`;
+}
+
+// Clear the guess input field
+function clearInput() {
+  const guessInput = document.getElementById("guessInput");
+  guessInput.value = "";
+}
+
+// Add a new entry to the leaderboard
+function addLeaderboardEntry() {
+  if (loggedInUser) {
+    const leaderboardEntry = {
+      username: loggedInUser.username,
+      attempts: attempts
+    };
+
+    leaderboard.push(leaderboardEntry);
     leaderboard.sort((a, b) => a.attempts - b.attempts);
 
-    // Display the leaderboard
-    showLeaderboard();
-
-    // Reset the game
-    attempts = 0;
-    randomNumber = generateRandomNumber(minRange, maxRange);
-    document.getElementById("guessInput").value = "";
-  } else {
-    alert(`Your guess is ${result}. Try again!`);
-  }
-}
-
-// Function to handle the username input
-function handleUsernameInput() {
-  username = document.getElementById("usernameInput").value;
-}
-
-// Function to handle the password input
-function handlePasswordInput() {
-  password = document.getElementById("passwordInput").value;
-}
-
-// Function to handle the login button click
-function handleLogin() {
-  if (!username || !password) {
-    alert("Please enter both username and password.");
-    return;
-  }
-
-  alert(`Logged in as ${username}`);
-  clearInputs();
-}
-
-// Function to handle the signup button click
-function handleSignup() {
-  if (!username || !password) {
-    alert("Please enter both username and password.");
-    return;
-  }
-
-  alert(`Signed up with username ${username}`);
-  clearInputs();
-}
-
-// Function to clear the input fields
-function clearInputs() {
-  document.getElementById("usernameInput").value = "";
-  document.getElementById("passwordInput").value = "";
-}
-
-// Function to handle the Enter key press
-function handleKeyPress(event) {
-  if (event.key === "Enter") {
-    if (document.activeElement === document.getElementById("guessInput")) {
-      handleGuess();
-    } else if (document.activeElement === document.getElementById("usernameInput")) {
-      handleSignup();
-    } else if (document.activeElement === document.getElementById("passwordInput")) {
-      handleSignup();
+    if (leaderboard.length > 10) {
+      leaderboard.pop();
     }
+
+    showLeaderboard();
   }
 }
 
-// Function to display the leaderboard
+// Display the leaderboard
 function showLeaderboard() {
   const leaderboardContainer = document.getElementById("leaderboard");
-  leaderboardContainer.innerHTML = ""; // Clear the existing content
+  leaderboardContainer.innerHTML = "";
 
   if (leaderboard.length > 0) {
-    const table = document.createElement("table");
+    const leaderboardTable = document.createElement("table");
+    leaderboardTable.classList.add("leaderboard-table");
+
     const headerRow = document.createElement("tr");
     const rankHeader = document.createElement("th");
     rankHeader.textContent = "Rank";
@@ -125,7 +105,7 @@ function showLeaderboard() {
     headerRow.appendChild(rankHeader);
     headerRow.appendChild(usernameHeader);
     headerRow.appendChild(attemptsHeader);
-    table.appendChild(headerRow);
+    leaderboardTable.appendChild(headerRow);
 
     leaderboard.forEach((entry, index) => {
       const row = document.createElement("tr");
@@ -138,11 +118,73 @@ function showLeaderboard() {
       row.appendChild(rankCell);
       row.appendChild(usernameCell);
       row.appendChild(attemptsCell);
-      table.appendChild(row);
+      leaderboardTable.appendChild(row);
     });
 
-    leaderboardContainer.appendChild(table);
+    leaderboardContainer.appendChild(leaderboardTable);
   } else {
     leaderboardContainer.textContent = "No entries yet.";
   }
 }
+
+// Handle login
+function handleLogin() {
+  const usernameInput = document.getElementById("usernameInput");
+  const passwordInput = document.getElementById("passwordInput");
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  if (username && password) {
+    loggedInUser = {
+      username: username,
+      password: password
+    };
+
+    updateMessage(`Welcome, ${username}!`);
+    clearInput();
+    usernameInput.disabled = true;
+    passwordInput.disabled = true;
+    showLeaderboard();
+  } else {
+    updateMessage("Please enter both username and password.");
+  }
+}
+
+// Handle signup
+function handleSignup() {
+  const usernameInput = document.getElementById("usernameInput");
+  const passwordInput = document.getElementById("passwordInput");
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  if (username && password) {
+    loggedInUser = {
+      username: username,
+      password: password
+    };
+
+    updateMessage(`Welcome, ${username}!`);
+    clearInput();
+    usernameInput.disabled = true;
+    passwordInput.disabled = true;
+    showLeaderboard();
+  } else {
+    updateMessage("Please enter both username and password.");
+  }
+}
+
+// Reset the game and clear user login
+function resetGame() {
+  startNewGame();
+  const usernameInput = document.getElementById("usernameInput");
+  const passwordInput = document.getElementById("passwordInput");
+  usernameInput.value = "";
+  passwordInput.value = "";
+  usernameInput.disabled = false;
+  passwordInput.disabled = false;
+}
+
+// Start a new game when the page loads
+window.onload = function() {
+  startNewGame();
+};
